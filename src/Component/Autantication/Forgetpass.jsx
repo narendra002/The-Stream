bc27007bc073bfafa6e82029ce54492e9b6c34d5
FIRST_NAME LAST_NAME <kushwahdivyansh2@gmail.com>
2023-01-18 11:03:20 +0530

    auth

--- src/Component/Autantication/Forgetpass.jsx
+++ src/Component/Autantication/Forgetpass.jsx
@@ -4,36 +4,33 @@ import OtpInput from 'react-otp-input';
 import TextField from '@mui/material/TextField';
 
 const Forgetpass = () => {
-   
-    const move=( totext)=>{
-            
-            document.getElementById(totext).focus();
-            
+
+    const move = (totext) => {
+
+        document.getElementById(totext).focus();
+
     }
 
-    const verify=()=>{
-      
+    const verify = () => {
+
     }
     return (
         <>
-             <div className='forgetmain'>
-             <div className='container'>
+            <div className='forgetmain'>
+                <div className='container'>
                     <div className='reset'>
-                       <h1>Reset password</h1>
-                       <TextField label="E-mail" variant="outlined" className='resetEmail'/>
-                      
-                        <div  class="code-container">
-                    
-                            <input type="text" class="otp" id='1' maxlength="1" onKeyUp={() => move( '2')}></input>
-                            <input type="text" class="otp" id='2' maxlength="1" onKeyUp={() => move( '3')}></input>
-                            <input type="text" class="otp" id='3' maxlength="1" onKeyUp={() => move( '4')}></input>
-                            <input type="text" class="otp" id='4' maxlength="1" onKeyUp={() => move( '5')}></input>
+                        <h1>Reset password</h1>
+                        <TextField label="E-mail" variant="outlined" className='resetEmail' />
+
+                        <div class="code-container">
+
+                            <input type="text" class="otp" id='1' maxlength="1" onKeyUp={() => move('2')}></input>
+                            <input type="text" class="otp" id='2' maxlength="1" onKeyUp={() => move('3')}></input>
+                            <input type="text" class="otp" id='3' maxlength="1" onKeyUp={() => move('4')}></input>
+                            <input type="text" class="otp" id='4' maxlength="1" onKeyUp={() => move('5')}></input>
                             <input type="text" class="otp" id='5' maxlength="1" ></input>
-
-
-                       </div>
-                       <button className='Verify' onClick={()=> verify()}>Verify</button>
-                       
+                        </div>
+                        <button className='Verify' onClick={() => verify()}>Verify</button>
                     </div>
                 </div>
             </div>
@@ -43,11 +40,4 @@ const Forgetpass = () => {
 
 
 };
-
-
-
-
-
-
-
 export default Forgetpass;
--- src/Component/Autantication/Signup.jsx
+++ src/Component/Autantication/Signup.jsx
@@ -5,22 +5,22 @@ import TextField from '@mui/material/TextField';
 const Signup = () => {
 
     return (
-    
-            <div className='signupmain'>
-               <div className='Signup_container'>
-                    <div className='Signup'>
-                       <h1>Sign up</h1>
-                       <TextField label="username" className='Email'     />
-                       <TextField label="E-mail" variant="outlined" className='Email'     />
-                       <TextField label=" Create Strong Password" variant="outlined" className='Email'  />
 
-                    </div>
-                    <div className='Alredyacc'>
-                        <h>Already have an account? <a href='./signin'>Login</a></h>
-                        <button className='Loginbtn'>Signup</button>
-                    </div>
+        <div className='signupmain'>
+            <div className='Signup_container'>
+                <div className='Signup'>
+                    <h1>Sign up</h1>
+                    <TextField label="username" className='Email' />
+                    <TextField label="E-mail" variant="outlined" className='Email' />
+                    <TextField label=" Create Strong Password" variant="outlined" className='Email' />
+
+                </div>
+                <div className='Alredyacc'>
+                    <h>Already have an account? <a href='./signin'>Login</a></h>
+                    <button className='Loginbtn'>Signup</button>
                 </div>
             </div>
+        </div>
     );
 };
 
