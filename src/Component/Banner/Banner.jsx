7cf979d0fb69d25e22824330610d61afd5acbe67
FIRST_NAME LAST_NAME <kushwahdivyansh2@gmail.com>
2023-02-26 21:37:20 +0530

    Master

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -1,19 +1,16 @@
-const PopularMovies = "popular";
-import React, { useState, useEffect } from 'react';
-import axios from "axios"
-const API_KEY = '4008ea8497eda5d3e80f32017f7d35bc';
-const url = "https://api.themoviedb.org/3";
+import React from 'react';
+import { useState } from 'react';
+
 const imgUrl = "https://image.tmdb.org/t/p/original";
 const Banner = ({ BannerType }) => {
-
-
-
-
 	const link = `${imgUrl}${BannerType?.backdrop_path}`;
 
-	return (
 
+
+	return (
 		<>
+
+
 			<div className="banner"
 				style={{ backgroundImage: `url('${link}')` }} >
 			</div>
@@ -22,7 +19,7 @@ const Banner = ({ BannerType }) => {
 				<br></br>
 				<h3 className="Banner_description">{BannerType.overview}</h3>
 				<div className='BannerButton'>
-					<button className='watchnow'>▶ Watch Now</button>
+					<button className='watchnow' >▶ Watch Now</button>
 					<button className='Detail'>Detail</button>
 				</div>
 			</div>
--- src/Component/Nav/Navbar.jsx
+++ src/Component/Nav/Navbar.jsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-// import { SearchIcon } from "react-icons/im";
-// import  from '@mui/icons-material/Search';
-import SearchIcon from '@mui/icons-material/Search';
+// import SearchIcon from '@mui/icons-material/Search';
+// import { ReactSearchAutocomplete } from 'react-search-autocomplete'
 import "./Navbar.scss"
+// import Autocomplete from '@mui/material/Autocomplete';
+// import Data from './Data.json'
+import { Card, colors, Input } from '@mui/material';
+import axios from "axios"
+// import { match } from 'assert';
+
 const Navbar = () => {
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [results, setResults] = useState([]);
+
+  function handleSearch(e) {
+    setSearchTerm(e.target.value);
+    if(searchTerm.length>2){
+      fetch(`https://balldontlie.io/api/v1/players/237/search?q=${searchTerm}`)
+      .then(res => res.json())
+      .then(data => {
+        setResults(data);
+        console.log(searchTerm);
+        });
+    }else{
+      setResults([]);
+    }
+  }
+    
   return (
+
+    
     <nav className='header'>
 
-     <div className='logoname'>
-     <h className="finallogo">The-Stream</h>
-     </div>
+      <div className='logoname'>
+        <h className="finallogo">The-Stream</h>
+      </div>
 
       <div className="line">
         <ul>
@@ -20,16 +45,38 @@ const Navbar = () => {
           <li><Link to="/TvShows">Tv Show</Link></li>
           <li><Link to="/Anime">Anime</Link></li>
           <li><Link to="/watch_list">Watch List</Link></li>
-
-
         </ul>
-
-      
       </div>
+
       <div class='searchcontainer' >
-          <SearchIcon className='serchicon' />
-          <input className='searchfield' placeholder='search' type='text'></input>
+
+         <Input
+          className='searchfield'
+          placeholder='search...'
+          style={{ width: 200, color: 'white', marginRight: 100 }}
+          value={searchTerm}
+          onChange={handleSearch}
+          // onChange={e => onChangeHangle(e.target.value)}
+          // value={text}
+        /> 
+         
+         <ul>
+        {results.map(item => (
+          <li key={item.id}>{item.city}</li>
+        ))}
+      </ul>
+             
+        <div >
+
+          {/* {search?.map((el, i) =>
+            <div key={i}>
+              <span>{el.name}</span>
+            </div>
+          )}
+          */}
         </div>
+
+      </div>
     </nav>
   )
 }
